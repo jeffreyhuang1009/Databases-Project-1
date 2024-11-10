f089286ea25bf61a3abad96f2526bfdd62d4a662
wfishell <wf2322@columbia.edu>
2024-11-10 16:50:22 -0500

    Update Packages.py

--- pages/Packages.py
+++ pages/Packages.py
@@ -2,5 +2,5 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import IntegrityError, DataError
-uri='postgresql://wf2322:St278-Ahobo$#cGHh@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/w4111'
+uri=''
 engine=create_engine(uri)
